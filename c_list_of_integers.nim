49187c8ff8378e158855328ece29bcfd3fcf4c03
vicfred <git@vicfred.dev>
2025-03-30 23:50:13 -0700

    list of integers

---
+++ c_list_of_integers.nim
@@ -0,0 +1,9 @@
+# https://atcoder.jp/contests/abc396/tasks/abc396_a
+# list of integers
+import sequtils, strutils
+
+let n = stdin.readLine.parseInt
+let a = stdin.readLine.split.map parseInt
+
+echo n
+echo a
